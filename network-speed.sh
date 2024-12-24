84fe99b18e1828b7ae09f0a9d2821e6b1efaaaa1
Roni Laukkarinen <roni@dude.fi>
2024-12-24 20:09:03 +0200

    Fix showing empty speeds

--- network-speed.sh
+++ network-speed.sh
@@ -5,28 +5,29 @@ CACHE_MAX_AGE=900
 
 # Function to format the output specifically for conky
 format_output() {
+  local output=""
   while IFS= read -r line; do
-    if [[ $line == *"Ping:"* ]]; then
-      echo "Internet ping: ${line#*: }"
-    elif [[ $line == *"Download:"* ]]; then
-      echo "Download speed: ${line#*: }"
-    elif [[ $line == *"Upload:"* ]]; then
-      echo "Upload speed: ${line#*: }"
+    if [[ $line == *"Ping:"* && ${line#*: } != "N/A" ]]; then
+      output+="Internet ping: ${line#*: }\n"
+    elif [[ $line == *"Download:"* && ${line#*: } != "N/A" ]]; then
+      output+="Download speed: ${line#*: }\n"
+    elif [[ $line == *"Upload:"* && ${line#*: } != "N/A" ]]; then
+      output+="Upload speed: ${line#*: }\n"
     fi
   done
+  if [ -n "$output" ]; then
+    echo -e "$output"
+  fi
 }
 
 # Function to get speed test results
 get_speed_test() {
   # Requires speedtest-cli package
   speedtest-cli --simple 2>/dev/null | format_output > "$SPEED_CACHE_FILE"
-  if [ $? -eq 0 ]; then
-    cat "$SPEED_CACHE_FILE"
-  else
-    echo "Internet Ping: N/A"
-    echo "Download Speed: N/A"
-    echo "Upload Speed: N/A"
+  if [ $? -ne 0 ]; then
+    echo "" > "$SPEED_CACHE_FILE"
   fi
+  cat "$SPEED_CACHE_FILE"
 }
 
 # Check if cache exists and is fresh (15 minutes)
